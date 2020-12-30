32a2b6818190f6806a98b2d31c53d77100b1286e
tangshiwu <1726213442@qq.com>
2020-12-30 15:56:49 +0800

    sync

--- app/src/main/java/com/ghl/memo/DataManager.java
+++ app/src/main/java/com/ghl/memo/DataManager.java
@@ -2,25 +2,47 @@ package com.ghl.memo;
 
 import androidx.room.Room;
 
+import com.ghl.memo.room.Memo;
+import com.ghl.memo.room.MemoDao;
 import com.ghl.memo.room.MemoDatabase;
 
+import java.util.List;
+
 public class DataManager {
+    private static MemoDatabase memoDatabase;
 
     private static DataManager instance;
 
     private DataManager() {
     }
 
+    //单例模式
     public static DataManager getInstance() {
         if (instance == null) {
             instance = new DataManager();
+            memoDatabase = Room.databaseBuilder(MemoApplication.getApplication(),
+                    MemoDatabase.class, "database-memo").allowMainThreadQueries().build();
         }
         return instance;
     }
 
-    public MemoDatabase getDb() {
-        return Room.databaseBuilder(MemoApplication.getApplication(),
-                MemoDatabase.class, "database-memo").build();
+    public MemoDao getMemoDao(){
+        return memoDatabase.memoDao();
     }
 
+    public void insertMemo(Memo memo){
+        getMemoDao().insert(memo);
+    }
+
+    public List<Memo> selectMemo(){
+        return getMemoDao().getAll();
+    }
+
+    public void updateMemo(Memo memo){
+        getMemoDao().update(memo);
+    }
+
+    public void deleteMemo(Memo memo){
+        getMemoDao().delete(memo);
+    }
 }
--- app/src/main/java/com/ghl/memo/room/MemoDatabase.java
+++ app/src/main/java/com/ghl/memo/room/MemoDatabase.java
@@ -3,7 +3,7 @@ package com.ghl.memo.room;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Memo.class}, version = 1)
+@Database(entities = {Memo.class}, version = 1, exportSchema = false)
 public abstract class MemoDatabase extends RoomDatabase {
     public abstract MemoDao memoDao();
 }
